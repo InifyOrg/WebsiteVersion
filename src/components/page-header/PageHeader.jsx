ea59fd59092e3424a7033985dc14485e57576264
Sergey Svist <sergeysvist557@gmail.com>
2023-10-13 14:39:35 +0300

    add: theme switcher and mini user profile to page header

--- src/components/page-header/PageHeader.jsx
+++ src/components/page-header/PageHeader.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import styles from "./PageHeader.module.css";
-import { Search } from 'react-bootstrap-icons';
+import { Person, PersonFill, Search } from 'react-bootstrap-icons';
 
 const PageHeader = () => {
     return (
         <div className={styles.pageHeader}>
-            <div className={styles.searchBackground}>
-                <input className={styles.searchInput} type="text" placeholder='Search address | username' />
+            <div className={styles.leftHeader}>
+                <div className={styles.searchBackground}>
+                    <input className={styles.searchInput} type="text" placeholder='Search address | username' />
+                </div>
+
+                <p className={styles.gradientText}>Current wallet:</p>
+                <p>&nbsp; All</p>
             </div>
 
-            <p className={styles.gradientText}>Current wallet:</p>
-            <p>&nbsp; All</p>
+            <div className={styles.rightHeader}>
+                <input type="checkbox" id="switch" /><label for="switch"></label>
+
+                <div className={styles.miniProfile}>
+                    <PersonFill size={40} color='#00A99D' className={styles.content}></PersonFill>
+                    <p className={styles.content}>Hello, Nick</p>
+                </div>
+            </div>
         </div>
     );
 }
