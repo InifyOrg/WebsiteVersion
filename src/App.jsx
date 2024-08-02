3e9016142d5f011b53b17796bd41d3b28e72ce44
Sergey Svist <sergeysvist557@gmail.com>
2024-08-02 21:59:25 +0300

    add: profile page and routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.css';
 import PageHeader from './components/page-header/PageHeader';
 import SideNavBar from './components/side-nav-bar/SideNavBar.jsx';
-import Main from './components/Main/Main.jsx';
+import ProfilePage from './components/profile-page/ProfilePage.jsx'
 
 function App() {
   return (
       <div className={styles.main}>
         <SideNavBar></SideNavBar>
         <PageHeader></PageHeader>
-        <Main></Main>
+
+        <div className={styles.content}>
+          <Routes>
+            <Route index path='/profile' element={<ProfilePage></ProfilePage>}></Route>
+          </Routes>
+        </div>
       </div>
   );
 }
---
+++ src/components/profile-page/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./ProfilePage.module.css";
+
+const PageHeader = () => {
+    return (
+        <div className={styles.profile}>
+            ProfilePage
+        </div>
+    );
+}
+
+export default PageHeader;
