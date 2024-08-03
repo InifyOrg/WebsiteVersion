8f6806513c84e9d2000fc6c2a8f007fdc2759dbf
oche <ronaldoruilc@gmail.com>
2024-08-03 18:21:19 +0300

    add: profile structure

--- src/components/profile-page/ProfilePage.jsx
+++ src/components/profile-page/ProfilePage.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from "./ProfilePage.module.css";
+import profile_banner from './imgs/banner.jpg';
 
 const PageHeader = () => {
     return (
         <div className={styles.profile}>
-            ProfilePage
+            <div className={styles.top_info}>
+                <div className={styles.profile_frame}>
+                    <div className={styles.profile_banner}>
+                        <img src={profile_banner} alt="" />
+                    </div>
+                </div>
+                <div className={styles.wallet_frame}>
+                
+                </div>
+            </div>
+            <div className={styles.portfolio}>
+                <div className={styles.portfolio_frame}>
+
+                </div>
+                <div className={styles.profit_frame}>
+
+                </div>
+            </div>
         </div>
     );
 }
