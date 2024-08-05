67cec363055cb2731e05819f498d5bfef18b1036
oche <ronaldoruilc@gmail.com>
2024-08-05 18:36:59 +0300

    add: profile_frame

--- src/components/profile-page/ProfilePage.jsx
+++ src/components/profile-page/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./ProfilePage.module.css";
-import profile_banner from './imgs/banner.jpg';
+import profile_banner from './imgs/banner2.jpg';
+import profile_photo from './imgs/profile_photo2.png';
 
 const PageHeader = () => {
     return (
@@ -9,6 +10,14 @@ const PageHeader = () => {
                 <div className={styles.profile_frame}>
                     <div className={styles.profile_banner}>
                         <img src={profile_banner} alt="" />
+                    </div>
+                    <div className={styles.profile_info}>
+                        <div className={styles.profile_photo}>
+                            <img src={profile_photo} alt="" />
+                        </div>
+                        <div className={styles.profile_name}>
+                        My name is oche
+                        </div>
                     </div>
                 </div>
                 <div className={styles.wallet_frame}>
